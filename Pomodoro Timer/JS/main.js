7387bf3a19e01e77e30d5dc83caaf74bf0d38909
Kabelo Mofokeng <67164248+nnyeo@users.noreply.github.com>
2023-10-07 13:41:21 +0200

    Full functional pomodoro time

--- Pomodoro Timer/JS/main.js
+++ Pomodoro Timer/JS/main.js
@@ -3,20 +3,39 @@ const stopEl = document.getElementById("stop");
 const resetEl = document.getElementById("reset");
 const timerEl = document.getElementById("timer");
 
-let interval
+let interval;
+let timeLeft = 1500;
+
+function updateTimer(){
+  let minutes = Math.floor(timeLeft/60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  timerEl.innerHTML = formattedTime;
+}
 
 function startTimer(){
   interval = setInterval(() => {
-    console.log("timer")
+    timeLeft--;
+    updateTimer();
+
+    if(timeLeft==0){
+      clearInterval(interval);
+      alert("Time's up!!");
+      timeLeft = 1500;
+    }
+    
   }, 1000);
 }
 
 function stopTimer(){
-  console.log("stop");
+  clearInterval(interval);
 }
 
 function resetTimer(){
-  console.log("reset");
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
 }
 
 
